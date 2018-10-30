07ed09881d1f2cdd0d91a381ced2239128182414
trubanovva <trubanovva@nspk.ru>
2018-10-30 11:42:15 +0300

    Added changes into AccountController.java

--- src/main/java/ru/home/restfulbankapi/controllers/AccountController.java
+++ src/main/java/ru/home/restfulbankapi/controllers/AccountController.java
@@ -1,5 +1,6 @@
 package ru.home.restfulbankapi.controllers;
 
+import lombok.extern.java.Log;
 import ru.home.restfulbankapi.models.Account;
 import ru.home.restfulbankapi.models.Currency;
 import ru.home.restfulbankapi.models.UserInfo;
@@ -9,23 +10,27 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
 
-@Path("myresource")
+@Path("account")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@Log
 public class AccountController {
 
     @POST
     @Path("/createAccount")
     public Response createAccount(UserInfo userInfo, @QueryParam("currency") Currency currency) {
+        if (userInfo == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("UserInfo is null").build();
+        }
+
+        if (currency == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Currency is null").build();
+        }
+
         Account account = new Account(userInfo, currency, new BigDecimal(0));
-        System.out.println(account.toString());
-
+        log.info(String.format("/createAccount : %s", account.toString()));
         return Response.ok(account).build();
-    }
-
-    @GET
-    @Path("/hello")
-    public Response test() {
-        return Response.ok("Hello").build();
     }
 }
