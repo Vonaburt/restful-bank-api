f87f2f73411fcb0396f98f5e587dffd8d9a5da5c
Vadim Trubanov <vonaburt@gmail.com>
2021-07-18 18:52:41 +0300

    Add test to AccountControllerTest.java

--- src/main/test/ru/home/restfulbankapi/AccountControllerTest.java
+++ src/main/test/ru/home/restfulbankapi/AccountControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import ru.home.restfulbankapi.models.Account;
 import ru.home.restfulbankapi.models.UserInfo;
 
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -12,6 +13,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Аккаунт")
 class AccountControllerTest {
 
     private static WebTarget target;
@@ -26,7 +30,7 @@ class AccountControllerTest {
     @Tag("functional")
     @DisplayName("Создание аккаунта. Корректные данные")
     @Test
-    void createAccountTest() {
+    void createAccountPositiveTest() {
         UserInfo userInfo = new UserInfo();
         userInfo.setFirstName("Kamilla");
         userInfo.setLastName("Kamillkina");
@@ -39,8 +43,29 @@ class AccountControllerTest {
                 .post(Entity.json(userInfo));
         Account actualAccount = createAccountResponse.readEntity(Account.class);
 
-        Assertions.assertEquals(actualAccount.getUserInfo(),
+        assertEquals(actualAccount.getUserInfo(),
                 userInfo, "UserInfo is not equals");
+    }
+
+    @Tag("functional")
+    @DisplayName("Создание аккаунта. Некорректные данные")
+    @Test
+    void createAccountNegativeTest() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setFirstName("");
+        userInfo.setLastName("");
+        userInfo.setAge((short) 24);
+
+        Response createAccountResponse = target.path("account")
+                .path("createAccount")
+                .queryParam("currency", "EUR")
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .post(Entity.json(userInfo));
+
+        Exception exception = assertThrows(ProcessingException.class, () ->
+                createAccountResponse.readEntity(Account.class));
+
+        assertTrue(exception.getMessage().contains("Error reading entity from input stream"), "Error message do not found");
     }
 
     @AfterAll
